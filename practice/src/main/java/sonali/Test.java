144c3ccf4a0f16d53f5986193a964c49dd13954b
Sonalidhomne7 <sonalidhomne7@gmail.com>
2020-06-15 02:39:15 +0530

    A program by using static keyword.

--- practice/src/main/java/sonali/Test.java
+++ practice/src/main/java/sonali/Test.java
@@ -1,9 +1,30 @@
 package sonali;
 
 public class Test {
-    public static void main(String args[])
-    {
-        System.out.println("Hi");
-        System.out.println("This is our first practice Test");
+
+    // non-static method
+   static int multiply(int a, int b){
+        return a * b;
+    }
+
+    // static method
+    static int add(int a, int b){
+        return a + b;
     }
 }
+
+    class Main {
+
+        public static void main( String[] args ) {
+
+            // create an instance of the StaticTest class
+            Test st = new Test();
+
+            // call the nonstatic method
+            System.out.println(" 2 * 2 = " + st.multiply(2,2));
+
+            // call the static method
+            System.out.println(" 2 + 3 = " + Test.add(2,3));
+        }
+    }
+
